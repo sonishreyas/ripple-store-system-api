9ce51902d0cb115f8dc86aa5eaf1be46c2eab63b
Shreyas Soni <sonishreyas10@gmail.com>
2022-03-08 02:30:50 +0530

    trial

--- src/api.js
+++ src/api.js
@@ -2,10 +2,15 @@ const express = require("express");
 
 const serverless = require("serverless-http");
 
-var cors = require("cors");
 var app = express();
+const cors = require("cors");
+const corsOptions = {
+	origin: "*",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 const router = express.Router();
 
