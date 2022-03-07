27734c206aadc6234fb3dc57e0bfb4b3e962729c
Shreyas Soni <sonishreyas10@gmail.com>
2022-03-08 02:04:02 +0530

    cores

--- src/api.js
+++ src/api.js
@@ -3,7 +3,30 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+	// Website you wish to allow to connect
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
 
+	// Request methods you wish to allow
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	);
+
+	// Request headers you wish to allow
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-Requested-With,content-type"
+	);
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader("Access-Control-Allow-Credentials", true);
+
+	// Pass to next layer of middleware
+	next();
+});
 const router = express.Router();
 
 router.get("/hi", (req, res) => {
@@ -14,18 +37,5 @@ router.get("/hi", (req, res) => {
 
 app.use("/.netlify/functions/api", router);
 
-app.use(function (req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "*"); //If it doesn't work, change it to "*"
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-	);
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-Requested-With,content-type,token"
-	);
-	res.setHeader("Access-Control-Allow-Credentials", true);
-	next();
-});
 module.exports = app;
 module.exports.handler = serverless(app);
