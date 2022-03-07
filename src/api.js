00b8b4baae51884f6c847a0917516d380034254b
Shreyas Soni <sonishreyas10@gmail.com>
2022-03-08 01:56:30 +0530

    cors added

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const app = express();
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/hi", (req, res) => {
 	res.json({
 		hello: "hi",
 	});
@@ -14,5 +14,18 @@ router.get("/", (req, res) => {
 
 app.use("/.netlify/functions/api", router);
 
+app.use(function (req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "*"); //If it doesn't work, change it to "*"
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-Requested-With,content-type,token"
+	);
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	next();
+});
 module.exports = app;
 module.exports.handler = serverless(app);
