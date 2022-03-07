59ca4679120473ba9ad90c5380093ad2ff7e0d85
Shreyas Soni <sonishreyas10@gmail.com>
2022-03-08 02:19:04 +0530

    all req enabled

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const serverless = require("serverless-http");
 
-const app = express();
-// Add headers before the routes are defined
 var cors = require("cors");
-app.use(cors({ origin: "*" }));
+var app = express();
+
+app.use(cors());
 
 const router = express.Router();
 
